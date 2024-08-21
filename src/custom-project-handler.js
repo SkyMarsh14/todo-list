4a6619d42ef907b96d4d24654a9338d4eb0c3cea
Sky Marsh <soras1758@gmail.com>
2024-08-21 13:42:30 +0900

    deleted all modules. starting clean except for main files.

--- src/custom-project-handler.js
+++
@@ -1,6 +0,0 @@
-import {projects} from "./util.js";
-
-const projectsContainer = document.querySelector('.projects');
-export default function displayProjectTasks(){
-    projects[1].printTask();
-}
