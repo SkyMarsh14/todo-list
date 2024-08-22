ca20a84b24d96041060d1de08a3dabe5f4b701f0
Sky Marsh <soras1758@gmail.com>
2024-08-22 20:02:30 +0900

    add message when there's no tasks in the projected clicked

--- src/DomControll/displayTasks.js
+++ src/DomControll/displayTasks.js
@@ -1,11 +1,14 @@
+const main = document.querySelector('main');
+const noTasks = document.createElement('div');
+noTasks.textContent="No tasks in this project :(";
+
 export default function displayTasks(project) {
-  
+  main.innerHTML = "";
   if(project.tasks.length===0){
-    console.error("No task assinged to the project");
+    main.append(noTasks);
     return ;
   }
-  document.querySelector("main").innerHTML = "";
-  
+
     project.tasks.forEach((task) => {
     const container = document.createElement("div");
     const title = document.createElement("h1");
