17c75055dd9869a9548a97727df61835c9b03ff0
Sky Marsh <soras1758@gmail.com>
2024-08-24 15:52:01 +0900

    styling to sidebar

--- src/DomControll/sidebarControll.js
+++ src/DomControll/sidebarControll.js
@@ -5,6 +5,5 @@ sidebarBtn.addEventListener("click", () => {
   const collapse = sidebar.dataset.collapse;
   if (collapse==="true") sidebar.dataset.collapse = "false";
   else sidebar.dataset.collapse = "true";
-
 });
 export {sidebarBtn}
